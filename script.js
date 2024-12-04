2c975926068a2bd6690ccc5c2b0ad343ffaa5cfd
Jungwoo Hong <hjw4963@gmail.com>
2024-12-05 00:42:18 +0100

    hide, show timer js update

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+document.getElementById('start_options').addEventListener('click', () => {
+    console.log('start_options');
+    document.getElementById('options').style.display = 'none';
+    document.getElementById('game').style.display = 'block';
+});
+
+
 let result;
 document.getElementById('start-btn').addEventListener('click', () => {
     console.log('start');
+    let selected_difficulty = document.querySelector('input[name="difficulty"]:checked').value;
+    if (selected_difficulty === 'withouttimer') {
+        document.getElementById('time').style.display = 'none';
+    } else {
+        document.getElementById('time').style.display = 'block';
+    }
+
     document.getElementById('stop-btn').disabled = false;
     document.getElementById('start-btn').disabled = true;
     let time = 0;
@@ -20,5 +34,6 @@ document.getElementById('start-btn').addEventListener('click', () => {
             result = Math.floor(result * 100) / 100;
             document.getElementById('result-message').textContent = 'your time is ' + result + ' seconds off';
         }
+        document.getElementById('time').style.display = 'block';
     });
 });

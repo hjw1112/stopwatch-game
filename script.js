93da21d2ed74ddd15e25ef36a6a7f4c14d19565a
Jungwoo Hong <hjw4963@gmail.com>
2024-12-04 23:46:20 +0100

    fixed js bug(except double timer bug) and minor change in html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let result;
 document.getElementById('start-btn').addEventListener('click', () => {
     console.log('start');
     document.getElementById('stop-btn').disabled = false;
@@ -12,11 +13,12 @@ document.getElementById('start-btn').addEventListener('click', () => {
         clearInterval(interval);
         document.getElementById('start-btn').disabled = false;
         document.getElementById('stop-btn').disabled = true;
+        if (time === 10) {
+            document.getElementById('result-message').textContent = 'You win!';
+        } else {
+            result = 10 - time;
+            result = Math.floor(result * 100) / 100;
+            document.getElementById('result-message').textContent = 'your time is ' + result + ' seconds off';
+        }
     });
-    if (time === 10) {
-        result = 10 - time;
-        document.getElementById('result-message').textContent = 'You win!';
-    } else {
-        document.getElementById('result-message').textContent = 'your time is ' + result + ' seconds off';
-    }
 });
